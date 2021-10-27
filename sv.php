aa699fede0f2b2ab253cf7bfde618448d81b29a1
Xuansa2012 <90166710+Xuansa2012@users.noreply.github.com>
2021-10-27 18:02:08 +0700

    Add files via upload
    giao diện sinh viên

---
+++ sv.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  	.tm:hover{box-shadow: 5px 5px 5px 5px #AAA;}
+  </style>
+</head>
+<body>
+	<div class="wrapper" style="">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">chăm học </a>
+  <div class="collapse navbar-collapse" id="">
+    <ul class="navbar-nav ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Môn học  <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Hỏi đáp </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">đề ôn tập </a>
+      </li>
+    </ul>
+    <div style="margin-left: 450px " >
+  	<input id="tipue_drop_input" class="input is-rounded" type="text" placeholder="Tìm kiếm" required="" value="">
+  </div>
+  <div style="margin-left: 5px;">
+  	3...
+  </div>
+  <div style="margin-left: 20px;">
+  	thông báo 
+  </div>
+   <div style=" background-color: red;margin-left:10px;width: 50px;">
+    ánhsv 
+  </div>
+   </div>
+  
+
+</nav>
+<div class="slide" style="width: 980px;height: 400px;background-color: green;margin-left: 50px;">
+
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+	  <div class="carousel-inner" style="width: 980px;height: 400px;background-color: green;">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="anh1.png" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="anh2.png" alt="Second slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="anh3.png" alt="Third slide">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</div>
+<div class="giaotrinh" style="width: 200px;height: 500px;background-color: yellow;margin-top: -400px;margin-left: 1030px;"></div>
+<div class="monhoc" style="width: 980px;height: 400px;margin-left: 50px;margin-top: -100px;">
+	<div class="monhoc1" style="width: 980px;height:100px;color:blue;font-size:30px;">Môn học</div>
+	<div class="cacmon" style="width: 980px;height: 300px;">
+		<div class="chitm" style="width: 980px;height: 150px;">
+			<div class="tm"style="width: 200px;height: 200px;float: left;">
+				<div class="pro-img"style="width: 200px;height: 150px; background-color: white;">
+					<img src="cnweb.png" style="width: 100%;height: 100%;">
+				</div>
+				<div class="text-pro">
+					Công nghệ web 
+				</div>
+			</div>
+			<div class="tm"style="width: 200px;height: 200px;float: left;margin-left: 20px;">
+				<div class="pro-img"style="width: 200px;height: 150px; background-color: white;">
+					<img src="thud.png" style="width: 100%;height: 100%;">
+				</div>
+				<div class="text-pro">
+					Thống kê ứng dụng 
+				</div>
+			</div>
+			<div class="tm"style="width: 200px;height: 200px;float: left;margin-left: 20px;">
+				<div class="pro-img"style="width: 200px;height: 150px; background-color: white;">
+					<img src="ttnt.png" style="width: 100%;height: 100%;">
+				</div>
+				<div class="text-pro">
+					Trí tuệ nhân tạo 
+				</div>
+			</div>
+			<div class="tm"style="width: 200px;height: 200px;float: left;margin-left: 20px;">
+				<div class="pro-img"style="width: 200px;height: 150px; background-color: white;">
+					<img src="anbmtt.png" style="width: 100%;height: 100%;">
+				</div>
+				<div class="text-pro">
+					An toàn bảo mật thông tin
+				</div>
+			</div>
+
+		 </div>
+	</div>
+</div>
+</div>
+<footer style="width: 100%;height: 80px;background-color: black; color:white;">
+	<div class="text"style="margin-left: 500px;padding-top: 20px;"> 
+		TRƯỜNG ĐẠI HỌC THỦY LỢI<br>
+		Khoa công nghệ thông tin
+	</div>
+</footer>
+</body>
+</html>
